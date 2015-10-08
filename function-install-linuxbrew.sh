009eb9fb7a80903cad26922c38f1139c0bf07a83
mordini <yahewisthedevil@yahoo.com>
2015-10-08 14:16:25 -0400

    single user linuxbrew install after adduser

--- function-install-linuxbrew.sh
+++ function-install-linuxbrew.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-. function-pause.sh
-. function-get-users.sh
+#. function-pause.sh
+#. function-get-users.sh
 
-IFS=' ' read -a users <<< getusers
+# add this functionality later
+# to get users, choose users, install for users
+#IFS=' ' read -a users <<< getusers
+#getUsers
+#echo $testVar
+#echo ${users[4]}
+#echo 'Choose users to have Linuxbrew:'
 
-echo ${users[4]}
+# for now, install on a per-user add basis
+function installLinuxBrew {
 
-echo 'Choose users to have Linuxbrew:'
+    userDir=$(eval echo ~$1)
+    echo $userDir
+    
+    su - $1 -c 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"'
+
+    sed -i '1i\\' $userDir/.bashrc
+    sed -i '1i export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"' $userDir/.bashrc
+    sed -i '1i export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"' $userDir/.bashrc
+    sed -i '1i export PATH="$HOME/.linuxbrew/bin:$PATH"' $userDir/.bashrc
+
+    su - $1 -c 'brew doctor'
+}
