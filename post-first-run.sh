1a18eb341a2df582795226a5ccc68898614ea463
mordini <yahewisthedevil@yahoo.com>
2015-10-08 16:43:19 -0400

    added emacs install, custom emacs

---
+++ post-first-run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#put scripts, installs, etc you want in here, new user!
+
+#i want emacs
+brew install emacs
+
+#these items must stay together
+echo 'WARNING! this will erase your current .emacs.d. if you don''t know what this means, I wouldn''t worry about it (go ahead)'
+echo ''
+echo 'install custom .emacs.d? y/n'
+
+read instCustEmacs
+
+if [ $instCustEmacs == 'y' ]; then
+    mkdir ~/.emacs.d
+    rm -rf ~/first-run-temp
+    git clone https://github.com/mordini/.emacs.d.git ~/first-run-temp
+    mv ~/first-run-temp/* ~/.emacs.d
+    rm -rf first-run-temp
+else
+    echo 'NYAH!'
+fi
