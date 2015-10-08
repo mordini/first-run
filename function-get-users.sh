5e6975191956c75130654ff6387b114986f081f5
mordini <yahewisthedevil@yahoo.com>
2015-10-08 14:16:45 -0400

    test functionality

--- function-get-users.sh
+++ function-get-users.sh
@@ -2,8 +2,18 @@
 
 function getUsers() {
 
+    #working on this one!
+    
     #awk -F':' '$2 ~ "\$" {print $1}' /etc/shadow
-    awk -F':' '$2 ~ "\$" {return $1}' /etc/shadow
+    declare -a userArray=($(awk -F':' '$2 ~ "\$" {print $1}' /etc/shadow))
+
+    #testing sending vars to another script
+    #sourced file
+    testVar='this is a test'
+ 
+    #echo ${userArray[0]}
+    #echo ${userArray[1]}
+    #echo ${userArray[2]}
 }
 
-getUsers
+#getUsers
